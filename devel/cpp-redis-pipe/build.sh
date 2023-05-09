c07e59ec523dc81eafa01b213583cabe34a9b2b0
Daniel Liebner <dliebner@gmail.com>
2023-05-09 07:13:34 -0400

    Fix cpp-redis-pipe/build.sh (checkout a specific commit) Add `set -ex` to install.sh (exit if command fails, print commands)

--- devel/cpp-redis-pipe/build.sh
+++ devel/cpp-redis-pipe/build.sh
@@ -6,6 +6,7 @@ rm -rf cpp_redis lib; mkdir lib
 
 (  git clone https://github.com/cpp-redis/cpp_redis
    cd cpp_redis
+   git checkout fc2c43fa07e45e1ef0f77cbda6a240e7890ae5aa
    git submodule init; git submodule update
    mkdir build; cd build
    cmake .. -D{CMAKE_BUILD_TYPE=Release,CMAKE_INSTALL_PREFIX=../../lib}
